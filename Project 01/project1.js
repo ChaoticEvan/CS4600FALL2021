39c370767adfa03eaa791259517f36eb994ca8eb
Evan Voordeckers <evan.voordeckers@yahoo.com>
2021-09-04 16:15:19 -0600

    Getting weird behvaior on star

--- Project 01/project1.js
+++ Project 01/project1.js
@@ -3,22 +3,32 @@
 // fgOpac is the opacity of the foreground image.
 // fgPos is the position of the foreground image in pixels. It can be negative and (0,0) means the top-left pixels of the foreground and background are aligned.
 function composite(bgImg, fgImg, fgOpac, fgPos) {
-    var bgWid = bgImg.width;
-    var bgHght = bgImg.height;
+    var posCount = 0;
+    var fgPos = 0;
+    var startingIdx = 0;
+    if (fgPos.y ** fgPos.x) {
+        startingIdx = (fgPos.y * bgImg.width + fgPos.x) * 4;
+    }    
 
-    var fgWid = fgImg.width;
-    var fgHght = fgImg.height;
+    for (var bgPos = startingIdx; bgPos < bgImg.data.length; bgPos += 4) {
+        if (posCount == fgImg.width ) {
+            bgPos += (bgImg.width * 4) - (fgImg.width * 4);
+            posCount = 0;
+        }
+        
+        var fgRed = fgImg.data[fgPos];
+        var fgGreen = fgImg.data[fgPos + 1];
+        var fgBlue = fgImg.data[fgPos + 2];
+        var fgAlpha = fgImg.data[fgPos + 3] / 255;
 
-    var startingIdx = (fgPos.y * bgWid + fgPos.x) * 4;
-    for (var i = startingIdx; i < fgImg.data.length; i += 4) {
-        var fgRed = fgImg.data[i];
-        var fgGreen = fgImg.data[i + 1];
-        var fgBlue = fgImg.data[i + 2];
-        var fgAlpha = fgImg.data[i + 3] / 255;
+        if (bgImg.data[bgPos + 3] != 0 && fgAlpha != 0) {
+            bgImg.data[bgPos] = (fgOpac * fgRed) + ((1 - fgOpac) * bgImg.data[bgPos]);
+            bgImg.data[bgPos + 1] = (fgOpac * fgGreen) + ((1 - fgOpac) * bgImg.data[bgPos + 1]);
+            bgImg.data[bgPos + 2] = (fgOpac * fgBlue) + ((1 - fgOpac) * bgImg.data[bgPos + 2]);
+            bgImg.data[bgPos + 3] = (fgAlpha + ((1 - fgAlpha) * (bgImg.data[bgPos + 3] / 255))) * 255;
+        }
 
-        bgImg.data[i] = (fgAlpha * fgRed) + ((1 - fgAlpha) * bgImg.data[i]);
-        bgImg.data[i + 1] = (fgAlpha * fgGreen) + ((1 - fgAlpha) * bgImg.data[i + 1]);
-        bgImg.data[i + 2] = (fgAlpha * fgBlue) + ((1 - fgAlpha) * bgImg.data[i + 2]);
-        bgImg.data[i + 3] = (fgAlpha + ((1 - fgAlpha) * (bgImg.data[i + 3] / 255))) * 255;
+        posCount++;
+        fgPos +=4;
     }
 }
