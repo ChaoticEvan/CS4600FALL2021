c33cb12e032120c87277ae1fd09d3b6f58d35649
Evan Voordeckers <evan.voordeckers@yahoo.com>
2021-09-12 00:13:34 -0600

    Math for scale is working

--- Project 02/project2.js
+++ Project 02/project2.js
@@ -3,7 +3,12 @@
 // The given rotation value is in degrees.
 function GetTransform( positionX, positionY, rotation, scale )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+	var x = Math.cos(rotation * (Math.PI / 180));
+	var y = Math.sin(rotation * (Math.PI / 180));
+	var n = scale * positionX;
+	var k = scale * positionY;	
+
+	return Array( x, y, 0, -y, x, 0, n, k, 1 );
 }
 
 // Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
